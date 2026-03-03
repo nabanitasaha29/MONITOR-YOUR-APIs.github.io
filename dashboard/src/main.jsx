f3c21bfaa8c44c2b6e9e42b6cb6a176fe8578a8b
Nabanita Saha <nsaha1604@gmail.com>
2026-03-03 12:50:28 +0530

    routing changed

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -6,7 +6,9 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* <BrowserRouter> */}
+
+    <BrowserRouter basename="/dashboard">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
--- dashboard/src/routes/SidebarRoutes.jsx
+++ dashboard/src/routes/SidebarRoutes.jsx
@@ -1,23 +1,43 @@
+// import React from "react";
+// import { Routes, Route, Navigate } from "react-router-dom";
+// import ApiMonitor from "../pages/APIMonitorPage/ApiMonitor.jsx";
+// import DahsboardLandingPage from "../pages/dashboard/DahsboardLandingPage.jsx";
+// import GroupsPage from "../pages/GroupsPage/GroupsPage.jsx";
+// import MonitorMainPage from "../pages/APIMonitorPage/MonitorMainPage.jsx";
+
+// const SidebarRoutes = () => {
+//   return (
+//     <Routes>
+
+//       <Route path="/" element={<Navigate to="/dashboard" replace />} />
+
+//       <Route path="/dashboard" element={<DahsboardLandingPage />} />
+//       <Route path="/api-monitor" element={<MonitorMainPage />} />
+//       <Route path="/groups" element={<GroupsPage />} />
+
+//       <Route path="*" element={<Navigate to="/dashboard" replace />} />
+//     </Routes>
+//   );
+// };
+
+// export default SidebarRoutes;
+
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import ApiMonitor from "../pages/APIMonitorPage/ApiMonitor.jsx";
 import DahsboardLandingPage from "../pages/dashboard/DahsboardLandingPage.jsx";
 import GroupsPage from "../pages/GroupsPage/GroupsPage.jsx";
-import MonitorMainPage from "../pages/APIMonitorPage/MonitorMainPage.jsx";
-
-
+//import MonitorMainPage from "../pages/APIMonitorPage/MonitorMainPage.jsx";
 
 const SidebarRoutes = () => {
   return (
     <Routes>
-      
-      <Route path="/" element={<Navigate to="/dashboard" replace />} />
+      <Route path="/" element={<Navigate to="/api-monitor" replace />} />
 
       <Route path="/dashboard" element={<DahsboardLandingPage />} />
-      <Route path="/api-monitor" element={<MonitorMainPage />} />
+      <Route path="/api-monitor" element={<ApiMonitor />} />
       <Route path="/groups" element={<GroupsPage />} />
 
-      
       <Route path="*" element={<Navigate to="/dashboard" replace />} />
     </Routes>
   );
